5cec09c23dcc991de1a095c9b79303bcb3a0a656
André Maciel <andretmaciel@gmail.com>
2021-03-11 18:16:25 -0300

    adding pbi files and scripts

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <iframe
+          width="1280"
+          height="720"
+          src="https://app.powerbi.com/view?r=eyJrIjoiZmI0NGYwYzgtMGRiZi00NDk0LWIyOGUtMjNkNjhlNDdlYjEyIiwidCI6ImEzYTc4ZDc4LWM5YzEtNGVkYi05Y2MyLTk2OWNjYWNjYWNkYiJ9&pageName=ReportSectionf0ea873116e3a337e94b"
+          frameborder="0"
+          allowFullScreen="true"
+        ></iframe>
+      </header>
+    </div>
+  );
+}
+
+export default App;
